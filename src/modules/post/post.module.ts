df5e3bceb5cc627a35fa12efebfc3c4727697f1a
ibnshayed <ibnshayed@gmail.com>
2023-12-28 22:22:56 +0600

    post search api added

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [PostController],
   providers: [PostService],
 })
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,10 +1,40 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { DataServices, PostEntity } from '../../repository';
 
 @Injectable()
 export class PostService {
-  constructor() {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly db: DataServices,
+  ) {}
 
-  searchPost(keyword: string) {
-    return 'searchPost';
+  async searchPost(keyword: string) {
+    const { data } = await this.httpService.axiosRef.get(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+
+    const postData: PostEntity = await this.db.post.findOne({
+      filter: {
+        keyword,
+      },
+    });
+
+    if (postData) {
+      return postData.posts;
+    }
+
+    const matchedPost = data.filter(
+      (post) => post.title.includes(keyword) || post.body.includes(keyword),
+    );
+
+    if (matchedPost.length) {
+      await this.db.post.createOne({
+        keyword,
+        posts: matchedPost,
+      });
+    }
+
+    return matchedPost;
   }
 }
